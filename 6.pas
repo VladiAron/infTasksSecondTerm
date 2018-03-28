76edab5643332c1a767115c8655a142ac95a1876
VAidarov <aidarovv@gmail.com>
2018-03-28 12:08:53 +0400

    raw

--- 6.pas
+++ 6.pas
@@ -1,37 +1,40 @@
-Program ZadachiSix3;
-const m = 10;
-const n = 4;
-type mass = array [1..m] of real;
-var matrix:mass;
+Program ZadachiSix4;
+const n = 20;
+type mass = array [1..n] of Real;
+var z,x:mass;
  
  
 
 procedure vvod(var a:mass);
 	var i:integer;
 	begin
-		for i:=1 to m do
-			a[i]:= (4 - random(8))*pi;
+		for i:=1 to n do
+		while (a[i] = 0) do
+			a[i]:= (100 - random(200));
 	end;
 
 procedure vivod(a:mass);
 	var i:integer;
 	begin
 		writeln();
-		for i:=1 to m do
+		for i:=1 to n do
+		begin
 			write(a[i]:4);
+			write(' ');
+		end;
 		writeln();
 	end;
 
 
-function summ(a:mass):real;
+function summ(a:mass):Real;
 	var i,k:integer;
 	b:mass;
 	begin
 		summ:=0;
-		for i:=1 to m do
+		for i:=1 to n do
 		begin
 			for k:=1 to n do
-				b[i]:=sin((a[i]*k)/k); 
+				b[i]:=sin(exp(k*(ln(a[i])))/k); 
 			summ:=summ+b[i];
 		end;
 	end;
