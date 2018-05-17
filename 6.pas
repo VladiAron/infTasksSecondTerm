13db19c74bfb9c08a4724beb797d7b52a4928b18
VAidarov <aidarovv@gmail.com>
2018-05-17 21:50:16 +0400

    working 6.7

--- 6.pas
+++ 6.pas
@@ -3,7 +3,8 @@ const n = 10;
 const m = 20;
 type table = array [1..n,1..m] of integer;
 type mass = array [1..n] of integer;
-var //type here smth
+var x:table;
+i:integer;
  
  
 procedure vvodO(var a:mass);
@@ -29,7 +30,7 @@ procedure vivodD(a:table);
 		begin
 			writeln();
 			for j:=1 to m do
-				write(a[i,j]:4);
+				write(a[i,j]:5);
 		end;
 	end;
 
@@ -41,8 +42,30 @@ procedure vivodO(a:mass);
 			writeln(a[i]);
 	end;
 
-
+function average(a:table; j:integer):Real;
+	var i,count:integer;
+	begin
+	average:=0;
+	count:=0;
+	for i:=1 to n do
+		if a[i,j] > 0 then
+		begin
+			average := average + a[i,j];
+			count:=count + 1;
+		end;
+	average:= average/count;
+	end;
 
 begin
 	randomize;
+	vvodD(x);
+	write('Дан массив');
+	vivodD(x);
+	writeln();
+	writeln('Среднее арифметическое каждого столбца');
+	for i:=1 to m do
+		writeln(average(x,i):5);
+	writeln();
 end.
+
+
