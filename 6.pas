271d250c4ef51adde97c9e3327cd49e068b90357
VAidarov <aidarovv@gmail.com>
2018-05-18 02:02:35 +0400

    working 6.9

--- 6.pas
+++ 6.pas
@@ -1,9 +1,9 @@
 Program ZadachiSix6;
 const n = 10;
-const m = 20;
-type table = array [1..n,1..m] of integer;
+const m = 15;
+type table = array [1..m,1..m] of integer;
 type mass = array [1..n] of integer;
-var //type here smth
+var x,y:table;//type here smth
  
  
 procedure vvodO(var a:mass);
@@ -13,22 +13,22 @@ procedure vvodO(var a:mass);
 			a[i]:=(100 - random(200));
 	end;
 
-procedure vvodD(var a:table);
+procedure vvodD(k,z:integer;  var a:table);
 	var i,j:integer;
 	begin
-		for i:=1 to n do
-			for j:=1 to m do 
+		for i:=1 to k do
+			for j:=1 to z do 
 				a[i,j]:= (100 - random(200));
 	end;
 
-procedure vivodD(a:table);
+procedure vivodD(k,z:integer;  a:table);
 	var i,j:integer;
 	begin
 		writeln();
-		for i:=1 to n do
+		for i:=1 to k do
 		begin
 			writeln();
-			for j:=1 to m do
+			for j:=1 to z do
 				write(a[i,j]:4);
 		end;
 	end;
@@ -41,8 +41,23 @@ procedure vivodO(a:mass);
 			writeln(a[i]);
 	end;
 
-
+procedure tvister(a:table; var b:table);
+var i,j:integer;
+begin
+	for i:=1 to n do
+		for j:=1 to m do
+			b[j,i]:=a[i,j];
+end;
 
 begin
 	randomize;
+	vvodD(n,m,x);
+	write('Дан массив');
+	vivodD(n,m,x);
+	writeln();
+	tvister(x,y);
+	write('Получился массив');
+	vivodD(m,n,y);
+	writeln();
+
 end.
