684020c84d0a159b1139c267c8d2b5d87b154a2a
VAidarov <aidarovv@gmail.com>
2018-05-30 01:56:13 +0400

    working 6.21

--- 6.pas
+++ 6.pas
@@ -1,43 +1,52 @@
-Program ZadachiSix20;
+Program ZadachiSix21;
 var x:Real; //type here smth
  
-procedure minFuncArg(var xmin:Real);
-var e,min,x,xmax:Real;
-a:integer;
+procedure minFuncArg(var maxx:Real);
+var e,max,x,xmax:Real;
+a,b,c:integer;
 begin
-	a:=random(100) - 50;
-	e:=0.2;
-	x:=0.2;
-	xmax:=10;
-	min:=a*x-ln(x);
+	a:=random(100);
+	b:=random(100);
+	c:=random(100);
+	e:=0.1;
+	x:=-2;
+	xmax:=2;
+	max:=a*exp(b*x+c*sqr(x));
 	while (x<=xmax) do
 	begin
-		if(min < a*x-ln(x)) then
+		if(max < a*exp(b*x+c*sqr(x))) then
 		begin
-			min:=a*x-ln(x);
-			xmin:=x;
+			max:=a*exp(b*x+c*sqr(x));
+			maxx:=x;
 		end;
 		x:=x+e;
 	end;
-	e:=0.01;
-	xmax:=xmin+0.2;
-	x:=xmin-0.2;
-	min:=a*x-ln(x);
-	while ((x<=xmax) and (min > a*x-ln(x))) do
+	e:=0.005;
+	xmax:=maxx+0.2;
+	x:=maxx-0.2;
+	max:=a*exp(b*x+c*sqr(x));
+	while ((x<=xmax) and (max > a*exp(b*x+c*sqr(x)))) do
 	begin
-		min:=a*x-ln(x);
-		xmin:=x;
+		if(max < a*exp(b*x+c*sqr(x))) then
+		begin
+			max:=a*exp(b*x+c*sqr(x));
+			maxx:=x;
+		end;
 		x:=x+e;
 	end;
 	writeln();
 	write('a = ');
 	writeln(a);
+	write('b = ');
+	writeln(b);
+	write('c = ');
+	writeln(c);
 	
 end; 
 
 begin
 	randomize;
 	minFuncArg(x);
-	write('Аргумент при котором функция принимает минимальное значение ');
+	write('Аргумент при котором функция принимает максимальное значение ');
 	writeln(x:4);
 end.
