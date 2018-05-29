b076d2eb65abc87d57187922536357e515efc721
VAidarov <aidarovv@gmail.com>
2018-05-30 03:59:13 +0400

    working l1 Номера четных эллементов массива

--- 6.pas
+++ 6.pas
@@ -1,48 +1,43 @@
-Program ZadachiSix6;
+Program ZadachiL1;
 const n = 10;
-const m = 20;
-type table = array [1..n,1..m] of integer;
 type mass = array [1..n] of integer;
-var //type here smth
+var x, evenNums:mass;
+count:integer; 
  
- 
-procedure vvodO(var a:mass);
+procedure vvodO(var a:mass; num:integer);
 	var i:integer;
 	begin
-		for i:=1 to n do
+		for i:=1 to num do
 			a[i]:=(100 - random(200));
 	end;
 
-procedure vvodD(var a:table);
-	var i,j:integer;
-	begin
-		for i:=1 to n do
-			for j:=1 to m do 
-				a[i,j]:= (100 - random(200));
-	end;
-
-procedure vivodD(a:table);
-	var i,j:integer;
-	begin
-		writeln();
-		for i:=1 to n do
-		begin
-			writeln();
-			for j:=1 to m do
-				write(a[i,j]:4);
-		end;
-	end;
-
-procedure vivodO(a:mass);
+procedure vivodO(a:mass; num:integer);
 	var i:integer;
 	begin
 		writeln();
-		for i:=1 to n do
+		for i:=1 to num do
 			write(a[i]:4);
 	end;
 
-
+procedure countAllEvenElts(var b:mass; a:mass; var count:integer);
+var i:integer;
+begin
+	for i:=1 to n do
+		if(a[i] mod 2 = 0)then
+		begin
+			count:=count+1;
+			b[count]:=i;
+		end;
+end;
 
 begin
 	randomize;
+	vvodO(x,n);
+	writeln('Данн массив');
+	vivodO(x,n);
+	countAllEvenElts(evenNums,x,count);
+	writeln();
+	writeln('Номера четных эллементов массива');
+	vivodO(evenNums, count);
+	writeln();
 end.
