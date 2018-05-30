766fed05efca6f7e0f71067090352f9c0bbe286b
VAidarov <aidarovv@gmail.com>
2018-05-30 21:19:37 +0400

    working 6.13

--- 6.pas
+++ 6.pas
@@ -1,18 +1,11 @@
-Program ZadachiSix6;
-const n = 10;
-const m = 20;
+Program ZadachiSix13;
+const n = 20;
+const m = 30;
 type table = array [1..n,1..m] of integer;
-type mass = array [1..n] of integer;
-var //type here smth
- 
- 
-procedure vvodO(var a:mass);
-	var i:integer;
-	begin
-		for i:=1 to n do
-			a[i]:=(100 - random(200));
-	end;
+var max, maxi, maxj:integer;
+x:table;
 
+ 
 procedure vvodD(var a:table);
 	var i,j:integer;
 	begin
@@ -33,16 +26,34 @@ procedure vivodD(a:table);
 		end;
 	end;
 
-procedure vivodO(a:mass);
-	var i:integer;
-	begin
-		writeln();
-		for i:=1 to n do
-			writeln(a[i]);
-	end;
-
-
+procedure maxElem(a:table; var max,maxi,maxj:integer);
+var i, j:integer;
+begin
+	max:=a[1,1];
+	maxi:=1;
+	maxj:=1;
+	for i:=1 to n do
+		for j := 1 to m do
+			if(a[i,j] > max) then
+			begin
+				max:=a[i,j];
+				maxi:=i;
+				maxj:=j;
+			end;
+end;
 
 begin
 	randomize;
+	vvodD(x);
+	writeln('Данна матрица');
+	vivodD(x);
+	maxElem(x, max, maxi, maxj);
+	writeln();
+	write('Максимальный элемент ');
+	writeln(max);
+	write('Строка ');
+	writeln(maxi);
+	write('Столбец ');
+	writeln(maxj);
+	writeln();
 end.
